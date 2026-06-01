0d7060c0b610b2c32f3fcdac7cd6cf78e3b3c5b0
intellcoder <chimeziejoshuac124@gmail.com>
2026-06-01 18:18:27 +0100

    updated origin

--- src/index.ts
+++ src/index.ts
@@ -16,13 +16,27 @@ import "./models/index";
 const app: Application = express();
 
 app.use("/webhook", webhookRouter);
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://noble-restaurant.ng",
+  "https://www.noble-restaurant.ng",
+];
+
 app.use(
   cors({
-    origin: [
-      "https://noble-restaurant-api.onrender.com",
-      "https://noble-restaurant.ng",
-    ],
+    origin: function (origin, callback) {
+      // Allow Postman/mobile apps/no-origin requests
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
